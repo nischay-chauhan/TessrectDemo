7be822cafe622c8011a2147402f5adceb4603003
nischay-chauhan <masternischay@outlook.com>
2024-03-09 12:43:06 +0530

    Routes setup

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import express from "express"
 import bodyParser from "body-parser"
 import dotenv from "dotenv"
+import imageRoute from "./routes/Image.route.js"
 const app = express()
 dotenv.config()
 app.use(bodyParser.json())
 
 
 const PORT = process.env.PORT || 3001
+
+app.use("/api/image", imageRoute)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`)
