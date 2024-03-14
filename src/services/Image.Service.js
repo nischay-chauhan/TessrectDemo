07e95669cc5a17f7e408f5e8565d34bd77c7b4a6
nischay-chauhan <masternischay@outlook.com>
2024-03-14 23:32:19 +0530

    Added the multer max file upload

--- src/services/Image.Service.js
+++ src/services/Image.Service.js
@@ -3,8 +3,8 @@ import fs from "fs"
 export const extractText = async (imagePath) => {
     try {
         const { data: { text } } = await Tesseract.recognize(imagePath, "eng");
-        fs.unlinkSync(filePath);
         return text;
+
     } catch (error) {
         console.error("Error extracting text from image:", error);
         throw error;
